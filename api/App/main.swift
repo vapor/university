8118e89236c418c1468dacd32c48d169ab6079e2
Tanner Nelson <me@tanner.xyz>
2016-07-15 15:29:47 -0400

    fix config issue

--- api/App/main.swift
+++ api/App/main.swift
@@ -1,11 +1,11 @@
 import Vapor
 import VaporMySQL
 
-let config = try Config(workingDirectory: "./")
-let user = config["database", "default", "user"] // FIXME: VaporMySQL should take care of this
+let _d = Droplet()
+let config = _d.config
 
 guard
-    let host = config["database", "default", "password"].string,
+    let host = config["database", "default", "host"].string,
     let user = config["database", "default", "user"].string,
     let password = config["database", "default", "password"].string,
     let database = config["database", "default", "database"].string
@@ -26,6 +26,17 @@ let drop = Droplet(providers: [mysql], preparations: [Tutorial.self])
 let tutorials = Tutorials()
 drop.resource("tutorials", tutorials)
 
+class NotFoundMiddleware: Middleware {
+    func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+        do {
+            return try next.respond(to: request)
+        } catch Abort.notFound {
+            throw Abort.custom(status: .notFound, message: "Resource not found.")
+        }
+    }
+}
+
+drop.globalMiddleware.append(NotFoundMiddleware())
 drop.globalMiddleware.append(Tutorial.Medium.Validator.Middleware()) // FIXME: drop.middleware
 
 drop.serve()
